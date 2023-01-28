bfe3a35d140c8ed9a793185ff76772670e705db8
SergeyMikheev <s.a.mikheev@yandex.ru>
2023-01-29 02:28:58 +0300

    apdate Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,3 +10,15 @@
 // 23432 -> да
 
 // НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ СТРОКИ
+
+int val1 = 14212;
+int val2 = 0;
+int t = val1;
+while (val1 != 0)
+{
+  int o = val1 % 10;
+  val1 = val1 / 10;
+  val2 = val2 * 10 + o;
+}
+Console.WriteLine(val2);
+Console.WriteLine(t == val2);
